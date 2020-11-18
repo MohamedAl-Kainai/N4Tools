c591f4fe172397592b4bd74796a07be7c7ca94b0
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2020-11-18 13:16:09 +0100

    test

--- N4Tools/N4Tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from N4Tools import Design
-from N4Tools.terminal import terminal
-
-__version__ = '1.7.0'
--- N4Tools/N4Tools/terminal.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-class terminal:
-    @property
-    def size(self): # terminal size
-        return {
-            'width':shutil.get_terminal_size().columns,
-            'height':shutil.get_terminal_size().lines,
-        }
