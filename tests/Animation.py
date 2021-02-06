995a34f73285628175291fb6d9277b4b90347a69
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-02-06 02:48:52 +0100

    Add Custom Animation to Animation tests

--- tests/Animation.py
+++ tests/Animation.py
@@ -4,11 +4,13 @@ from N4Tools.Design import (
 	Animation,
 	AnimationTools
 )
-import requests
+import requests, time
 
 A = Animation()
 
-@ThreadAnimation(Animation=A.Prograsse,kwargs={'min':0,'max':30})
+
+# exapmle 1
+@ThreadAnimation(Animation=A.Prograsse,kwargs={'min':0,'max':10})
 def check_url(Thread,urls):
 	result = []
 	for index,url in enumerate(urls):
@@ -24,6 +26,7 @@ def check_url(Thread,urls):
 check_url(['https://google.com']*10)
 
 
+# example 2
 text_animation = AnimationTools.set_text_anim('Loading...')
 @ThreadAnimation(Animation=A.Loading,kwargs={'text':text_animation})
 def check_url(Thread,urls):
@@ -38,3 +41,26 @@ def check_url(Thread,urls):
 	print (style[:-1])
 
 check_url(['https://google.com']*10)
+
+
+# example 3
+def check_url(urls):
+	def CustomAnimation(min=0,max=len(urls),**kwargs):
+		yield A.Prograsse(min=min,max=max,**kwargs)[0]+f'({round(min*100/max,1)}/100.0) '
+
+	@ThreadAnimation(Animation=CustomAnimation)
+	def check_url(Thread):
+		result = []
+		for index, url in enumerate(urls):
+			Thread.set_kwargs(min=index + 1, max=len(urls))
+			req = requests.get(url)
+			result.append([url, req.ok])
+		style = ''
+		for url, state in result:
+			style += url + ': ' + str(state) + '\n'
+		Thread.kill()
+		print(style[:-1])
+
+	check_url()
+
+check_url(['https://google.com']*500)
